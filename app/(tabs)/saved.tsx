4a66b96947f6ccff8b53e499b010384c12aa3fa5
Filiberto <filiberto.dev@gmail.com>
2023-12-12 01:27:10 +0100

    fix: locations state

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,184 +1,147 @@
-import { RefreshControl, StyleSheet, ScrollView, Pressable, Alert } from "react-native";
+import {
+	RefreshControl,
+	StyleSheet,
+	ScrollView,
+	Pressable,
+	Alert,
+	GestureResponderEvent,
+} from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import Location from "../../components/Location";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { FlatList } from "react-native-gesture-handler";
 
-type CardProps = {
-    name: string;
-    qta?: number;
-};
-
-const Card = function ({ name, qta }: CardProps) {
-    return (
-        <>
-            <Text>
-                {name} - {qta}
-            </Text>
-        </>
-    );
-};
+const savedPlacesFromLocalDB = [
+	{ id: "1", name: "Vittorio Veneto", value: 21.08, pinned: false },
+	{ id: "2", name: "Mestre", value: 19.71, pinned: true },
+	{ id: "3", name: "45.3661, 11.6649", value: 15.18, pinned: true },
+	{ id: "3", name: "45.3661, 11.6649", value: 15.18, pinned: true },
+	{ id: "3", name: "45.3661, 11.6649", value: 15.18, pinned: true },
+	{ id: "3", name: "45.3661, 11.6649", value: 15.18, pinned: true },
+];
 
 export default function TabTwoScreen() {
+	// Data for FlatList
+	const [places, setPlaces] = useState<typeof savedPlacesFromLocalDB>(
+		savedPlacesFromLocalDB
+	);
 
-    // Data for FlatList
-    const [Places, setPlaces] = useState([
-        { key: 1, name: "Vittorio Veneto", 
-          value: 21.08, pinned: false },
-        { key: 2, name: "Mestre", 
-          value: 19.71, pinned: true },
-        { key: 3, name: "45.3661, 11.6649", 
-          value: 15.18, pinned: true },
-    ])
+	const handleOnPressAdd = (event: GestureResponderEvent) => {
+		console.log("add location");
+		//set state su places (setPlaces)
+	};
+	const handleTogglePin = (locationId: string) => {
+		//set state su places (setPlaces)
+		console.log({ locationId });
+	};
 
-    // set refresh value
-    const [refreshing, setRefreshing] = React.useState(false)
-    // handle refresh List
-    const onRefreshSaved = React.useCallback(
-        ()=>{
-            setRefreshing(true)
-            const newVal = Math.floor(Math.random() * (25 - 10) + 10)
-            
-            // Per testare il toggle del pin
-            Places.forEach( (i) => { 
-                console.log(i) 
-                console.log("\n")
-            }) 
-            console.log("\n\n\n")
+	return (
+		<View style={styles.container}>
+			{/* Controls the generic page refresh */}
 
-            // console.log("\n\n\n")
+			{/* Header with button */}
+			<View style={styles.header}>
+				<Text style={styles.title}>Luoghi salvati</Text>
+				<Pressable style={styles.button} onPress={handleOnPressAdd}>
+					<Text style={styles.textButton}>Add location</Text>
+				</Pressable>
+			</View>
+			{/* Separator*/}
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			{/* FlatList of Location(s) */}
+			{/* 			<FlatList
+				keyExtractor={(item, index) => index.toString()}
+				data={places}
+				extraData={places} // ultra importante per vedere quando i dati cambiano
+				contentContainerStyle={{
+					alignContent: "center",
+					marginTop: 20,
+				}}
+				renderItem={({ item }) => {
+					if (item.pinned == false) {
+						//deleteListItem(item.id - 1)
 
-            setTimeout( () => {
-                // opzionale per i test
-                Places.push( 
-                    {
-                        key: (Places.length + 1),
-                        name: 'Place ' + (Places.length + 1).toString(),
-                        value: Math.floor(Math.random()  *  25 + (25 - 10)),
-                        pinned: true,
-                    }
-                )
-                setRefreshing(false)
-            }, 1500)
+						console.log("\nI love this\n");
+					}
 
-        }, []
-    );
+					return (
+						<Location
+							k={item.id}
+							name={item.name}
+							pinned={item.pinned}
+							value={item.value}
+							onTogglePinned={(index, value) => {
+								item.pinned = value ? false : true;
+							}}
+						></Location>
+					);
+				}}
+				ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+			></FlatList> */}
 
-    return (
-        <View
-        style={styles.container} 
-        >
-            { /* Controls the generic page refresh */ }
-            <RefreshControl
-                style={{ flex: 1,}}
-                refreshing={refreshing}
-                onRefresh={onRefreshSaved}
-                colors={ ['#FF002B', '#0066FF', '#00FF11'] }
-            >
-            
-                { /* Header with button */ }
-                <View style={styles.header}>
-                    <Text style={styles.title}>Luoghi salvati</Text>
-                    <Pressable 
-                    style={ styles.button } 
-                    onPress={ onRefreshSaved }
-                    >
-                        <Text style={styles.textButton}>Clicca qui</Text>
-                    </Pressable>
-                </View>
-                { /* Separator*/ }
-                <View
-                    style={styles.separator}
-                    lightColor="#eee"
-                    darkColor="rgba(255,255,255,0.1)"
-                />
-                { /* FlatList of Location(s) */ }
-                <FlatList
-                keyExtractor={(item, index) => index.toString() }
-                data={Places}
-                extraData={Places} // ultra importante per vedere quando i dati cambiano
-                contentContainerStyle={
-                    { 
-                        alignContent: 'center',
-                        marginTop: 20,
-                    }
-                }
-                renderItem={ ( { item } ) => {
-
-                    if( item.pinned == false ){
-                        //deleteListItem(item.key - 1)
-
-                        console.log("\nI love this\n")
-                    }
-
-                    return (
-                    <Location
-                    k={ item.key }
-                    name={ item.name }
-                    pinned={ item.pinned } 
-                    value={ item.value }
-                    onTogglePinned={ (index, value) => {
-                        item.pinned = value ? false : true
-                    }}  
-                    ></Location>
-                    ) } 
-                }
-                ItemSeparatorComponent={() => <View style={{height: 20}} />}
-    
-                >
-                </FlatList>
-
-            </RefreshControl>
-            
-            {/* <Card name="test" /> */}
-            {/* <EditScreenInfo path="app/(tabs)/three.tsx" /> */}
-        </View>
-    );
+			<ScrollView>
+				{places.map((place, index) => (
+					<View style={{ marginBottom: 20 }} key={index}>
+						<Location
+							id={place.id}
+							name={place.name}
+							pinned={place.pinned}
+							value={place.value}
+							onTogglePinned={handleTogglePin}
+						/>
+					</View>
+				))}
+			</ScrollView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        /* alignItems: "center",
+	container: {
+		flex: 1,
+		/* alignItems: "center",
         justifyContent: "center", */
-        padding: 20,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-        textAlign: 'center'
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        /* borderColor: 'orange',
+		padding: 20,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	header: {
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		/* borderColor: 'orange',
         borderWidth: 4, */
-        padding: 20,
-
-    },
-    button: {
-        backgroundColor: 'blue',
-        borderColor: 'grey',
-        borderWidth: 4,
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 3,
-    },
-    textButton: {
-        fontSize: 20,
-        fontWeight: '600',
-        height: '25%',
-        textAlign: 'center',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-        //color: 'white'
-    },
+		padding: 20,
+	},
+	button: {
+		backgroundColor: "blue",
+		borderColor: "grey",
+		borderWidth: 4,
+		borderRadius: 15,
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 3,
+	},
+	textButton: {
+		fontSize: 20,
+		fontWeight: "600",
+		height: "25%",
+		textAlign: "center",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+		//color: 'white'
+	},
 });
--- components/Location.tsx
+++ components/Location.tsx
@@ -1,114 +1,98 @@
-import { StyleSheet, TouchableOpacity } from 'react-native'
-import { Text, View,  } from './Themed'
-import { useState } from 'react'
-import { PinIcon, PinOff } from 'lucide-react-native';
-import { RefreshControl } from 'react-native-gesture-handler';
-
-
-
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View } from "./Themed";
+import { useState } from "react";
+import { PinIcon, PinOff } from "lucide-react-native";
+import { RefreshControl } from "react-native-gesture-handler";
 
 type LocationProps = {
-    name: string,
-    pinned: boolean,
-    value: number,
-    k: number,
-    onTogglePinned: (key: number, value: boolean) => void,
-}
-
-function truncateNumber( num: number ) {
-    return Math[num < 0 ? 'ceil' : 'floor'](num);
+	id: string;
+	name: string;
+	pinned: boolean;
+	value: number;
+	onTogglePinned: (locationId: string) => void;
 };
 
-export default function Location( props: LocationProps ){
+function truncateNumber(num: number) {
+	return Math[num < 0 ? "ceil" : "floor"](num);
+}
 
-    const k = props.k
-    const name = props.name
-    const [pinned, setPinned] = useState(props.pinned)
-    //const [value, setValue] = useState(0)
-    const value = props.value
-    const onToggle = props.onTogglePinned
+export default function Location(props: LocationProps) {
+	const name = props.name;
+	const [pinned, setPinned] = useState(props.pinned);
+	//const [value, setValue] = useState(0)
+	const value = props.value;
+	const onToggle = props.onTogglePinned;
 
-    var colorValue
+	var colorValue;
 
-    if( value < 16 ){
-        colorValue = 'red' // '#f2003c'
-    } else if( value >= 16 && value <= 20){
-        colorValue = '#ffda00' 
-    } else {
-        colorValue = 'green' // '#32cd32'
-    }
+	if (value < 16) {
+		colorValue = "red"; // '#f2003c'
+	} else if (value >= 16 && value <= 20) {
+		colorValue = "#ffda00";
+	} else {
+		colorValue = "green"; // '#32cd32'
+	}
 
-    return (
-        <View  
-        lightColor='#eee'
-        darkColor="rgba(255,255,255,0.1)"
-        >
-            { /* Item body */ }
-            <View style={ styles.body }
-            >
-                { /* Saved place name */}
-                <Text 
-                style={ styles.place }
-                >{ name }</Text>
-                { /* Pollution rate */}
-                <Text 
-                style={ 
-                    { 
-                        fontSize: 16,
-                        fontWeight: '600',
-                        color: colorValue
-                    }
-                }
-                >{ truncateNumber(value * 100) / 100 }</Text>
-                { /* Pressable Icon, toggles places to make them pinned or unpinned */}
-                <TouchableOpacity
-                style={styles.item}
-                activeOpacity={0} //
-                onPress={ () => { 
-                    pinned === true ? setPinned(false): setPinned(true) 
-                    onToggle( k, pinned )
-                } } 
-                
-                >
-                    { pinned ? 
-                        <PinIcon style={ styles.icon } size={24} color={'#f2003c'} /> 
-                            : 
-                        <PinOff  style={ styles.icon } size={24} color={'#f2003c'}/> 
-                    }
-
-                </TouchableOpacity>
-                
-            </View>
-        </View>
-    )
-
+	return (
+		<View lightColor="#eee" darkColor="rgba(255,255,255,0.1)">
+			{/* Item body */}
+			<View style={styles.body}>
+				{/* Saved place name */}
+				<Text style={styles.place}>{name}</Text>
+				{/* Pollution rate */}
+				<Text
+					style={{
+						fontSize: 16,
+						fontWeight: "600",
+						color: colorValue,
+					}}
+				>
+					{truncateNumber(value * 100) / 100}
+				</Text>
+				{/* Pressable Icon, toggles places to make them pinned or unpinned */}
+				<TouchableOpacity
+					style={styles.item}
+					activeOpacity={0} //
+					onPress={() => props.onTogglePinned(props.id)}
+				>
+					{pinned ? (
+						<PinIcon
+							style={styles.icon}
+							size={24}
+							color={"#f2003c"}
+						/>
+					) : (
+						<PinOff
+							style={styles.icon}
+							size={24}
+							color={"#f2003c"}
+						/>
+					)}
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    item: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        
-    },
-    body: {
-        flex: 1,
-        flexDirection: 'row',
-        borderColor: 'white', // light: #eee, dark: rgba(255,255,255,0.1)
-        borderWidth: 4,
-        borderRadius: 15,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 20,
-    },
-    place: {
-        fontSize: 16,
-        fontWeight: '600', // Semibold: 600
-    },
-    value: {
-        
-    },
-    icon: {
-        
-    }
-
+	item: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	body: {
+		flex: 1,
+		flexDirection: "row",
+		borderColor: "white", // light: #eee, dark: rgba(255,255,255,0.1)
+		borderWidth: 4,
+		borderRadius: 15,
+		alignItems: "center",
+		justifyContent: "space-between",
+		padding: 20,
+	},
+	place: {
+		fontSize: 16,
+		fontWeight: "600", // Semibold: 600
+	},
+	value: {},
+	icon: {},
 });
