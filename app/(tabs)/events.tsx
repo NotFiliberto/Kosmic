511482167b8553c64b0d492feee4c5b9fd191f27
Filiberto <filiberto.dev@gmail.com>
2023-12-12 16:47:39 +0100

    feat: show modal when pressing an event

--- app/(tabs)/events.tsx
+++ app/(tabs)/events.tsx
@@ -1,26 +1,54 @@
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 
 import EventCard from "../../components/common/EventCard";
 import { ScrollView } from "react-native-gesture-handler";
 
 import eventList from "../../assets/data/eventi.json";
 import { Event } from "@/lib/types";
+import EventModal from "../../components/common/EventModal";
+import { useState } from "react";
 
-const events = eventList as unknown as Event[];
 //test
+const events = eventList as unknown as Event[];
 
-export default function TabTwoScreen() {
+export default function EventScreen() {
+    const [eventModal, setEventModal] = useState<{
+        event: Event | undefined;
+        show: boolean;
+    }>({ event: undefined, show: false });
+
+    const handleEventOnPress = (event: Event) => {
+        console.log(event);
+        setEventModal({ event: event, show: true });
+    };
+
+    const handleModalOnClose = () => {
+        console.log("closing event modal...");
+        const { show, ...rest } = eventModal;
+        setEventModal({ ...rest, show: false });
+    };
+
     return (
         <ScrollView style={styles.container}>
+            <EventModal
+                isVisible={eventModal.show}
+                event={eventModal.event}
+                onClose={handleModalOnClose}
+            />
+
             {events.map((event, index) => (
-                <View style={{ marginBottom: 20 }} key={index}>
+                <Pressable
+                    style={{ marginBottom: 20 }}
+                    key={index}
+                    onPress={() => handleEventOnPress(event)}
+                >
                     <EventCard
                         name={event.name}
                         text={event.text}
                         date={new Date(event.date)}
                         url=""
                     />
-                </View>
+                </Pressable>
             ))}
         </ScrollView>
     );
