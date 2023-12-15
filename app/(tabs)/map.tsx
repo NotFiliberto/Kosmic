797b27ba90ab247eec347c1f6b3a105bdefd48f0
Filiberto <filiberto.dev@gmail.com>
2023-12-15 15:48:55 +0100

    fix: marker value

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -5,40 +5,39 @@ import { Text, View } from "../../components/Themed";
 import punti from "../../assets/data/valori_atlante_veneto.json";
 import { wPoint } from "../../assets/data/types";
 
-
 const points = punti as wPoint[];
 
 import MapView, {
-    Marker,
-    LatLng,
-    Region,
-    MapOverlay,
-    Overlay,
-    Heatmap,
-    MapMarker,
-    MarkerPressEvent,
-    Point,
-    LongPressEvent,
+	Marker,
+	LatLng,
+	Region,
+	MapOverlay,
+	Overlay,
+	Heatmap,
+	MapMarker,
+	MarkerPressEvent,
+	Point,
+	LongPressEvent,
 } from "react-native-maps";
 import InteractiveMap, { MarkerData } from "../../components/InteractiveMap";
 import MapPanel from "../../components/MapPanel";
 import { Key } from "lucide-react-native";
 
 export default function MapScreen() {
-    var initialRegion = {
-        latitude: 46,
-        longitude: 12,
-        latitudeDelta: 3,
-        longitudeDelta: 1,
-    };
+	var initialRegion = {
+		latitude: 46,
+		longitude: 12,
+		latitudeDelta: 3,
+		longitudeDelta: 1,
+	};
 
-    const [region, setRegion] = useState<Region>({
-       latitude: 46,
-       longitude: 12,
-       latitudeDelta: 3,
-       longitudeDelta: 1,
-     });
-    /* var markers = [
+	const [region, setRegion] = useState<Region>({
+		latitude: 46,
+		longitude: 12,
+		latitudeDelta: 3,
+		longitudeDelta: 1,
+	});
+	/* var markers = [
         {
             id: 1,
             coordinate: { latitude: 45, longitude: 12 },
@@ -47,123 +46,120 @@ export default function MapScreen() {
         // Add more markers as needed
     ]; */
 
-    const [markers, setMarkers] = useState<
-        {
-            id: number;
-            coordinate: { latitude: number; longitude: number };
-            title: string;
-        }[]
-    >([]);
+	const [markers, setMarkers] = useState<
+		{
+			id: number;
+			coordinate: { latitude: number; longitude: number };
+			title: string;
+		}[]
+	>([]);
 
+	const [selectedMarker, setSelectedMarker] = useState<MapMarker | undefined>(
+		undefined
+	);
 
-    const [selectedMarker, setSelectedMarker] = useState<MapMarker | undefined>(
-        undefined
-    );
-    
-    const mapRef = useRef<MapView>(null);
+	const mapRef = useRef<MapView>(null);
 
-    const [pollutionRate, setPollutionRate] = useState<number>()
+	const [pollutionRate, setPollutionRate] = useState<number>();
 
-    /* useEffect(() => {
+	/* useEffect(() => {
         console.log(selectedMarker);
     }, [selectedMarker]); */
 
-    function getWeight(pos: LatLng): number {
-        var ret = 0
-        console.log("pos:")
-        console.log(pos)
-        for (const point of points){
-            if(pos.latitude<= point.Y && pos.longitude>=point.X)
-                console.log("found:")
-                console.log(point.Y, point.X)
-                return point.Valore;
-        }
-        
-        
-        return ret;
+	function getWeight(pos: LatLng): number {
+		var ret = 0;
+		console.log("pos:");
+		console.log(pos);
+		for (const point of points) {
+			if (pos.latitude <= point.Y && pos.longitude >= point.X)
+				console.log("found:");
+			console.log(point.Y, point.X);
+			return point.Valore;
+		}
+		return ret;
+	}
 
-        
+	function onMarkerPress(event: MarkerPressEvent): void {
+		console.log(event);
+	}
 
-    }
+	function onMapPress(): void {
+		setSelectedMarker(undefined);
+		setMarkers([]);
+		console.log("deselected");
+	}
 
-    function onMarkerPress(event: MarkerPressEvent): void {
-        console.log(event)   
-    }
+	function onLongPress(event: LongPressEvent): void {
+		var lat = event.nativeEvent.coordinate.latitude;
+		var lng = event.nativeEvent.coordinate.longitude;
 
-    function onMapPress(): void {
-        setSelectedMarker(undefined);
-        setMarkers([]);
-        console.log("deselected");
-    }
-
-    function onLongPress(event: LongPressEvent): void {
-        var lat = event.nativeEvent.coordinate.latitude;
-        var lng = event.nativeEvent.coordinate.longitude;
-        /* markers.pop();
+		var newRegion = {
+			latitude: lat,
+			longitude: lng,
+			latitudeDelta: 3,
+			longitudeDelta: 1,
+		};
+		/* markers.pop();
         markers.push({
             id: 1,
             coordinate: { latitude: lat, longitude: lng },
             title: "My Marker",
         }); */
 
-        setMarkers([
-            {
-                id: 1,
-                coordinate: { latitude: lat, longitude: lng },
-                title: "My Marker",
-            },
-        ]);
-        //setSelectedMarker(event.nativeEvent.coordinate);
+		console.log("event: ", { event });
 
-        setRegion({
-          latitude: lat,
-          longitude: lng,
-          latitudeDelta: 3,
-          longitudeDelta: 1
-        })
+		setMarkers([
+			{
+				id: 1,
+				coordinate: { latitude: lat, longitude: lng },
+				title: "My Marker",
+			},
+		]);
+		//setSelectedMarker(event.nativeEvent.coordinate);
 
-        setPollutionRate(getWeight({latitude: lat, longitude: lng}))
+		setRegion(newRegion);
 
-        //console.log(pollutionRate);
+		setPollutionRate(getWeight({ latitude: lat, longitude: lng }));
 
-        mapRef.current?.animateToRegion(region)
-        mapRef.current?.render()
-        //console.log(selectedMarker)
-    }
+		//console.log(pollutionRate);
 
-    //console.log("map: ");
-    //console.log(region);
+		mapRef.current?.animateToRegion(newRegion);
+		mapRef.current?.render();
+		//console.log(selectedMarker)
+	}
 
-    return (
-        <View style={{ flex: 1 }}>
-            <InteractiveMap
-                initialRegion={initialRegion}
-                markers={markers}
-                onMarkerPress={onMarkerPress}
-                onMapPress={onMapPress}
-                onLongPress={onLongPress}
-                mapRef={mapRef}
-                region={region}
-                pollRate = {pollutionRate}
-            />
-        </View>
-    );
+	//console.log("map: ");
+	//console.log(region);
+
+	return (
+		<View style={{ flex: 1 }}>
+			<InteractiveMap
+				initialRegion={initialRegion}
+				markers={markers}
+				onMarkerPress={onMarkerPress}
+				onMapPress={onMapPress}
+				onLongPress={onLongPress}
+				mapRef={mapRef}
+				region={region}
+				pollRate={pollutionRate}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    title: {
-        fontSize: 200,
-        fontWeight: "bold",
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
-    },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	title: {
+		fontSize: 200,
+		fontWeight: "bold",
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
 });
-
