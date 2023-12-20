c67c167d26e9d66fcaf01cc52685b8bfe382bed3
VineetCesca <97135027+VineetCesca@users.noreply.github.com>
2023-12-20 10:00:11 +0100

    chore: backup before pull dev

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -11,10 +11,6 @@ import Location from "../../components/Location";
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
-
-
 // List data: id's must be unic at the beginning
 const savedPlacesFromLocalDB = [
 	{
@@ -35,80 +31,23 @@ const savedPlacesFromLocalDB = [
 	{ id: "11", name: "Villorba", value: 21.08, pinned: true },
 ];
 
-export default function LocationScreen() {
-	// Data for ScrollView and State usage
-	const [places, setPlaces] = useState<typeof savedPlacesFromLocalDB>(
-		savedPlacesFromLocalDB
-    );
-
-    const [unic, setUnic] = React.useState(places.length + 1) 
+export default function LocationScreen ()
+{
     
-    const addListItemRandom = () => {
-        setUnic( unic + 1 )
-        const id = unic.toString()
-        const num = Math.floor( Math.random() * 25  + 15 )
-        const s = 'Place ' + num.toString()
-        const item = { id: id, name: s, value: num, pinned: true }
-        const copy = places
-        copy.push(item)
-        setPlaces([...copy])
-    }
-
-	// Update any prop setting a specified value in a specified position in the List
-	const updateListItem = (
-		index: number,
-		nameProp: string,
-		value: any | undefined
-	) => {
-		// strategy: copy -> make changes: [index].prop = newValye -> set([...copy])
-		const copy = places;
-		copy[index] = { ...copy[index], [nameProp]: value }; // Remember that props names are seen as a list here
-		// remember its'a list -> []
-		setPlaces([...copy]);
-    };
-    
-    // Delete item with specified id
-    const deleteListItem = ( index: number ) =>
-    {
-        const copy = places
-        copy.splice(index, 1)
-        setPlaces( [ ...copy ])  
-    }
-
-
-	// Finds the position in data of an item through its specified ID
-	const findPositionItem = (id: string | undefined) => {
-		return places.findIndex((place) => place.id == id);
-	};
-
-	const getCurrentPinned = (index: number) => {
-		const copy = places;
-		return copy[index].pinned;
-	};
-
-	// handles when Pin icon gets toggled
-	const handleTogglePin = (id: string) => {
-		// solution to handle pintoggle (use icon handler where there is the click handler)
-		const position = findPositionItem(id);
-		const currentPinned = getCurrentPinned(position);
-        updateListItem( position, "pinned", !currentPinned );
-        deleteListItem(position)
-	};
-    
-
     // ASYNC STORAGE STUFF
-
-    // To understand if app is first launched
-    const [ loading, setLoading ] = useState( true )
-    const [ viewedOnBoarding, setViewedOnBoarding ] = useState( false )
 
     /* 
         Converte l'array savedPlacesFromLocalDB in una stringa JSON e la salva con la 
         chiave @savedPlaces in AsyncStorage. Se il salvataggio ha successo, viene 
         visualizzato un messaggio di log.
     */
-    const saveData = async () => {
-        try {
+    const saveData = async (item : { id: string, name: string, value: number, pinned: boolean} | null) => {
+        try
+        {
+            if ( item !== null && item )
+            {
+                addListItem(item)
+            }
             await AsyncStorage.setItem( '@savedPlaces', JSON.stringify( places ) );
             console.log('Dati salvati correttamente.');
         } catch (error) {
@@ -122,18 +61,22 @@ export default function LocationScreen() {
     */
     const getData = async () => {
         try {
-        const data = await AsyncStorage.getItem('@savedPlaces');
-        if (data !== null) {
-            const parsedData = JSON.parse( data );
-            console.log( 'Dati recuperati con successo: ' + (parsedData.length == 0 ? 'Last session cleaned all local storage' : 'n elem in storage: ' + parsedData.length) )
-            // Quando faccio la get, la faccio al primo accesso e metto i dati precedenti
-            // quelli che avevo salvato in locale l'ultima volta
-            let copy = places
-            copy = parsedData
-            setPlaces( copy );
-        }
-        } catch (error) {
-        console.error('Errore nel recupero dei dati:', error);
+            const data = await AsyncStorage.getItem( '@savedPlaces' );
+            if ( data !== null )
+            {
+                const parsedData = JSON.parse( data )
+                console.log( 'Dati recuperati con successo: ' + (parsedData.length == 0 ? 'Last session cleaned all local storage' : 'n elem in storage: ' + parsedData.length) )
+                // Quando faccio la get, la faccio al primo accesso e metto i dati precedenti
+                // quelli che avevo salvato in locale l'ultima volta
+                /* let copy = places
+                copy = parsedData
+                setPlaces([...copy]) */
+            }
+          
+        } catch ( error )
+        {
+            console.error( 'Errore nel recupero dei dati:', error );
+
         }
     };
 
@@ -148,10 +91,87 @@ export default function LocationScreen() {
     }
 
 
-    // Esegui getData all'avvio dell'app
-    useEffect(() => {
-        getData();
-    }, []); // L'array vuoto assicura che useEffect venga eseguito solo una volta
+	// Data for ScrollView and State usage
+    const [ places, setPlaces ] = useState<typeof savedPlacesFromLocalDB>(
+        []
+    );
+
+    const [unic, setUnic] = React.useState( places ? places.length + 1 : 0) 
+    
+    // Add a specified place
+    const addListItem = ( item: { id: string, name: string, value: number, pinned: boolean } ) =>
+    {
+        // Check id before add
+        setUnic( unic + 1 )
+        item.id = unic.toString()
+        const copy = places
+        if ( copy )
+        {
+            copy.push( item )
+            setPlaces( [ ...copy ] )
+        }
+        saveData(null)
+    }
+
+    // Add a random place
+    const addListItemRandom = () => {
+        setUnic( unic + 1 )
+        const id = unic.toString()
+        const num = Math.floor( Math.random() * 25  + 15 )
+        const s = 'Place ' + unic.toString()
+        const item = { id: id, name: s, value: num, pinned: true }
+        const copy = places
+        if ( copy )
+        {
+            copy.push(item)
+            setPlaces([...copy])
+        } 
+    }
+
+	// Update any prop setting a specified value in a specified position in the List
+	const updateListItem = (
+		index: number,
+		nameProp: string,
+		value: any | undefined
+	) => {
+		// strategy: copy -> make changes: [index].prop = newValye -> set([...copy])
+        const copy = places;
+        if( copy )
+		{copy[index] = { ...copy[index], [nameProp]: value }; // Remember that props names are seen as a list here
+		// remember its'a list -> []
+		setPlaces([...copy]);}
+    };
+    
+    // Delete item with specified id
+    const deleteListItem = ( index: number ) =>
+    {
+        const copy = places
+        if ( copy )
+        {copy.splice(index, 1)
+        setPlaces( [ ...copy ] )  
+        saveData(null)}
+    }
+
+
+	// Finds the position in data of an item through its specified ID
+	const findPositionItem = (id: string | undefined) => {
+		return places && places.findIndex((place) => place.id == id);
+	};
+
+	const getCurrentPinned = (index: number) => {
+		const copy = places;
+		return copy && copy[index].pinned;
+	};
+
+	// handles when Pin icon gets toggled
+	const handleTogglePin = (id: string) => {
+		// solution to handle pintoggle (use icon handler where there is the click handler)
+        const position = findPositionItem( id );
+        if( position )
+		{const currentPinned = getCurrentPinned(position);
+        updateListItem( position, "pinned", !currentPinned );
+        deleteListItem(position)}
+	};
 
         
     
@@ -162,7 +182,7 @@ export default function LocationScreen() {
             <View style={ styles.header }>
                 <Pressable
                 style={ [ styles.button_storage, { backgroundColor: 'red' } ]  }
-                onPress={ () =>{ saveData() } }
+                onPress={ () =>{ saveData(null) } }
             >
                 <Text style={ styles.text_button_storage }
                 >SAVE LOCAL</Text>
@@ -184,7 +204,7 @@ export default function LocationScreen() {
             </View>
             <ScrollView style={ styles.container } >
                 {
-                    places.map( ( place, index ) =>
+                    places && places.map( ( place, index ) =>
                     {
                         return ( place.pinned &&
                             <View style={ styles.item }
